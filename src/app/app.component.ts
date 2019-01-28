05de41f3b14c852cc6e2713a20b80da0d639f209
wecode <umurangamclaire@gmail.com>
2019-01-28 10:33:12 +0200

    add all changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import {Quotes} from './quotes'
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'Quotes';
+    goals = [
+        new Quotes(1,'Watch Finding Nemo'),
+        new Quotes(2,'Buy Cookies'),
+        new Quotes(3, 'Get new Phone Case'),
+        new Quotes(4, 'Get Dog Food'),
+        new Quotes(5, 'Solve math homework'),
+        new Quotes(6, 'Plot my world domination plan'),
+
+    ]
 }
