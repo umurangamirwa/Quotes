f34675b01b403aaf9c3d846f494f704e2b349618
wecode <umurangamclaire@gmail.com>
2019-01-28 14:18:03 +0200

    add changes

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
 import {Quotes} from '../quotes'
 
 @Component({
@@ -7,8 +7,15 @@ import {Quotes} from '../quotes'
   styleUrls: ['./quote-details.component.css']
 })
 export class QuoteDetailsComponent implements OnInit {
-@Input() quote:Quotes
+@Input() quote:Quotes;
+@Output() isComplete= new EventEmitter<boolean>();
+
+  quoteComplete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
   constructor() { }
+
+
 
   ngOnInit() {
   }
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -8,17 +8,19 @@ import {Quotes} from '../quotes'
 })
 export class QuotesComponent implements OnInit {
 
-    quotes = [
-        new Quotes(1,'Watch Finding Nemo','You teach best what you most need to learn'),
-        new Quotes(2,'Buy Cookies','The best way to pay for a lovely moment is to enjoy it'),
-        new Quotes(3, 'Get new Phone Case','Be happy for this moment. This moment is your life'),
-        new Quotes(4, 'Get Dog Food','Life is 10% what happens to you and 90% how you react to it'),
-        new Quotes(5, 'Solve math homework','Live life to the fullest and focus on the positive'),
-        new Quotes(6, 'Plot my world domination plan','I love the way Pharell is laying down great drum tracks. He is a great drummer'),
+    Quotes = [
+        new Quotes(1,'I think every band is a little cautious when the drummer starts to write tunes','You teach best what you most need to learn'),
+        new Quotes(2,'I show up ready to play, so I normally try and fit the situation','The best way to pay for a lovely moment is to enjoy it'),
+        new Quotes(3, 'Only I can change my life. No one can do it for me','Be happy for this moment. This moment is your life'),
+        new Quotes(4, 'Keep your eyes on the stars, and your feet on the ground','Life is 10% what happens to you and 90% how you react to it'),
+        new Quotes(5, 'Choose a job you love, and you will never have to work a day in your life','Live life to the fullest and focus on the positive'),
+        new Quotes(6,'Do not take life too seriously. You will never get out of it alive' ,'I love the way Pharell is laying down great drum tracks. He is a great drummer'),
+     
     ]
-
+    toogleDetails(index){
+      this.Quotes[index].showDescription = !this.Quotes[index].showDescription;
+  }
   constructor() { }
-
   ngOnInit() {
   }
 }
