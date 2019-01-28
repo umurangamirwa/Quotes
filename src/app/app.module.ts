d05f06214965b5fcccae8708f7a3166f031d1fa5
wecode <umurangamclaire@gmail.com>
2019-01-28 12:41:10 +0200

    add all changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuotesComponent } from './quotes/quotes.component';
+// import { QuotesDetailsComponent } from './quote-details/quote-details.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+//  import { QuotesComponent } from './quotes/quotes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesComponent,
+    // QuotesDetailsComponent,
+    QuoteDetailsComponent,
+     QuotesComponent
   ],
   imports: [
     BrowserModule,
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,19 +1,24 @@
-import { Component } from '@angular/core';
-import {Quotes} from './quotes'
+import { Component, OnInit } from '@angular/core';
+import {Quotes} from '../quotes'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
+  selector: 'app-quote',
+  templateUrl: './quotes.component.html',
+  styleUrls: ['./quotes.component.css']
 })
-export class AppComponent {
-    Quotes = [
-        new Quotes(1,'Watch Finding Nemo','You teach best what you most need to learn'), 
+export class QuotesComponent implements OnInit {
+
+    quotes = [
+        new Quotes(1,'Watch Finding Nemo','You teach best what you most need to learn'),
         new Quotes(2,'Buy Cookies','The best way to pay for a lovely moment is to enjoy it'),
         new Quotes(3, 'Get new Phone Case','Be happy for this moment. This moment is your life'),
         new Quotes(4, 'Get Dog Food','Life is 10% what happens to you and 90% how you react to it'),
         new Quotes(5, 'Solve math homework','Live life to the fullest and focus on the positive'),
         new Quotes(6, 'Plot my world domination plan','I love the way Pharell is laying down great drum tracks. He is a great drummer'),
-       
-
     ]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
 }
