d3dcf1c5910d7a5cdd3db4610b3155b60fb239e1
wecode <umurangamclaire@gmail.com>
2019-01-28 15:14:35 +0200

    add changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,12 @@ import {Quotes} from './quotes'
 })
 export class AppComponent {
     Quotes = [
-        new Quotes(1,'Watch Finding Nemo','You teach best what you most need to learn'), 
-        new Quotes(2,'Buy Cookies','The best way to pay for a lovely moment is to enjoy it'),
-        new Quotes(3, 'Get new Phone Case','Be happy for this moment. This moment is your life'),
-        new Quotes(4, 'Get Dog Food','Life is 10% what happens to you and 90% how you react to it'),
-        new Quotes(5, 'Solve math homework','Live life to the fullest and focus on the positive'),
-        new Quotes(6, 'Plot my world domination plan','I love the way Pharell is laying down great drum tracks. He is a great drummer'),
+        new Quotes(1,'Watch Finding Nemo','You teach best what you most need to learn',new Date(2005,3,4)), 
+        new Quotes(2,'Buy Cookies','The best way to pay for a lovely moment is to enjoy it',new Date(2005,2,3)),
+        new Quotes(3, 'Get new Phone Case','Be happy for this moment. This moment is your life',new Date(2005,4,3)),
+        new Quotes(4, 'Get Dog Food','Life is 10% what happens to you and 90% how you react to it',new Date(2005,7,8)),
+        new Quotes(5, 'Solve math homework','Live life to the fullest and focus on the positive',new Date(2005,8,9)),
+        new Quotes(6, 'Plot my world domination plan','I love the way Pharell is laying down great drum tracks. He is a great drummer',new Date(2005,9,8)),
        
 
     ]
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,19 +9,19 @@ import {Quotes} from '../quotes'
 export class QuotesComponent implements OnInit {
 
     Quotes = [
-        new Quotes(1,'I think every band is a little cautious when the drummer starts to write tunes','You teach best what you most need to learn'),
-        new Quotes(2,'I show up ready to play, so I normally try and fit the situation','The best way to pay for a lovely moment is to enjoy it'),
-        new Quotes(3, 'Only I can change my life. No one can do it for me','Be happy for this moment. This moment is your life'),
-        new Quotes(4, 'Keep your eyes on the stars, and your feet on the ground','Life is 10% what happens to you and 90% how you react to it'),
-        new Quotes(5, 'Choose a job you love, and you will never have to work a day in your life','Live life to the fullest and focus on the positive'),
-        new Quotes(6,'Do not take life too seriously. You will never get out of it alive' ,'I love the way Pharell is laying down great drum tracks. He is a great drummer'),
+        new Quotes(1,'I think every band is a little cautious when the drummer starts to write tunes','You teach best what you most need to learn',new Date(2002,3,4)),
+        new Quotes(2,'I show up ready to play, so I normally try and fit the situation','The best way to pay for a lovely moment is to enjoy it',new Date(2002,4,6)),
+        new Quotes(3, 'Only I can change my life. No one can do it for me','Be happy for this moment. This moment is your life',new Date(2002,6,4)),
+        new Quotes(4, 'Keep your eyes on the stars, and your feet on the ground','Life is 10% what happens to you and 90% how you react to it',new Date(2002,3,8)),
+        new Quotes(5, 'Choose a job you love, and you will never have to work a day in your life','Live life to the fullest and focus on the positive',new Date(2002,2,5)),
+        new Quotes(6,'Do not take life too seriously. You will never get out of it alive' ,'I love the way Pharell is laying down great drum tracks. He is a great drummer',new Date(2002,8,7)),
      
     ]
-    completeGoal(isComplete,index){
-      if (isComplete){
-          this.Quotes.splice(index,1);
-          }
-          }
+    // completeGoal(isComplete,index){
+    //   if (isComplete){
+    //       this.Quotes.splice(index,1);
+    //       }
+    //       }
 
 
 //     toogleDetails(index){
